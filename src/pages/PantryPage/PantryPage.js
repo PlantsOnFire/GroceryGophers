bf4a01e5740d9c432a179bb7ff53daeac5458bc6
Ashley Owen <ashleyaowen@hotmail.com>
2024-03-05 19:30:45 -0500

    pantry banner

--- src/pages/PantryPage/PantryPage.js
+++ src/pages/PantryPage/PantryPage.js
@@ -1,9 +1,11 @@
 import './PantryPage.scss';
 import PantryForm from '../../components/PantryForm/PantryForm';
+import {ReactComponent as PantryBanner} from '../../assets/PantryBanner.svg';
 
 function PantryPage () {
   return (
     <main>
+      <PantryBanner />
       <div className='container'>
         <h1>Pantry Page</h1>
         <h2>This is what you have in your pantry</h2>
