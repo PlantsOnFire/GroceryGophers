f40f772c90f6d60f71386ca8addfc9d4959e7d5e
info3170-Christine <cbottone88@gmail.com>
2024-03-25 10:05:59 -0400

    Error, About, Contact Pages Updated

--- src/pages/ContactPage/ContactPage.js
+++ src/pages/ContactPage/ContactPage.js
@@ -1,17 +1,19 @@
 import './ContactPage.scss';
 import ContactBanner from '../../components/Banners/ContactBanner';
+import ContactForm from '../../components/ContactForm/ContactForm';
 
 function ContactPage () {
   return(
     <main>
       <ContactBanner/>
       <div className='container p-5'>
-        <h2>More Content Goes Here</h2>
-        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim porttitor elit in cursus. Donec massa est, ultricies vitae semper dictum, euismod eget leo. Phasellus tincidunt eget ex eget ornare. Fusce condimentum velit non felis blandit lacinia. Donec tincidunt auctor turpis eu eleifend. Integer imperdiet convallis posuere. Sed tempor tortor quam, sit amet interdum purus molestie sed. Aliquam eget erat eget mauris suscipit efficitur. Praesent pellentesque tincidunt magna, a ultrices metus porta nec. Quisque posuere hendrerit sapien nec commodo. In rutrum nec libero a venenatis. Mauris molestie ligula id nibh aliquam, et lobortis massa semper. Etiam venenatis accumsan tortor, vitae mollis nisl suscipit sit amet. Ut turpis neque, placerat vitae elit eget, auctor luctus libero. Nulla consectetur mi eu arcu egestas laoreet.</div>
-      </div>
-      <div className='container p-5'>
-        <h2>More Content Goes Here</h2>
-        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim porttitor elit in cursus. Donec massa est, ultricies vitae semper dictum, euismod eget leo. Phasellus tincidunt eget ex eget ornare. Fusce condimentum velit non felis blandit lacinia. Donec tincidunt auctor turpis eu eleifend. Integer imperdiet convallis posuere. Sed tempor tortor quam, sit amet interdum purus molestie sed. Aliquam eget erat eget mauris suscipit efficitur. Praesent pellentesque tincidunt magna, a ultrices metus porta nec. Quisque posuere hendrerit sapien nec commodo. In rutrum nec libero a venenatis. Mauris molestie ligula id nibh aliquam, et lobortis massa semper. Etiam venenatis accumsan tortor, vitae mollis nisl suscipit sit amet. Ut turpis neque, placerat vitae elit eget, auctor luctus libero. Nulla consectetur mi eu arcu egestas laoreet.</div>
+        <h2>Contact The Gopher Team</h2>
+        <div>Having Troubles with the site? <br/>
+        Our team of gophers are here to help!<br/>
+        Just fill out the form below and we will get back to you in a timely manner.</div>
+        <br/>
+        <h2>Contact Form</h2>
+        <ContactForm/>
       </div>
     </main>
   )
--- src/pages/PageNotFound/PageNotFound.js
+++ src/pages/PageNotFound/PageNotFound.js
@@ -1,9 +1,18 @@
+import { NavLink } from 'react-router-dom';
+import PageErrorBanner from '../../components/Banners/PageErrorBanner';
 import './PageNotFound.scss';
 
 function PageNotFound () {
   return(
     <main>
-      PAGE NOT FOUND
+      <PageErrorBanner/>
+      <div className='container p-5'>
+        <h2>Error 404</h2>
+        <div>
+          <p>Sorry this page doesn't exist click the button below to return to Home!</p><br/>
+          <NavLink to={'/'}>Home Page</NavLink>
+        </div>
+      </div>
     </main>
   )
 }
